4e579829cbddf0814fe5aff0ef95e53d5f4f56c8
Jeff Arnold <jeffrey.arnold@gmail.com>
2011-02-05 16:19:04 -0500

    refactoring

--- colorbrewer.el
+++ colorbrewer.el
@@ -220,63 +220,63 @@
 
      ;; org-mode
      ;; (org-agenda-clocking ((t ())))
-;; org-agenda-column-dateline
-;; org-agenda-date
-;; org-agenda-date-today
-;; org-agenda-date-weekend
-;; org-agenda-diary
-;; org-agenda-dimmed-todo-face
-;; org-agenda-done
-;; org-agenda-restriction-lock
-;; org-agenda-structure
-;; org-archived
-;; org-block
-;; org-checkbox
-;; org-checkbox-statistics-done
-;; org-checkbox-statistics-todo
-;; org-clock-overlay
-;; org-code
-;; org-column
-;; org-column-title
-;; org-date
-;; org-document-info
-;; org-document-info-keyword
-;; org-document-title
-;; org-done
-;; org-drawer
-;; org-ellipsis
-;; org-footnote
-;; org-formula
-;; org-headline-done
-;; org-hide
-;; org-latex-and-export-specials
-;; org-level-1
-;; org-level-2
-;; org-level-3
-;; org-level-4
-;; org-level-5
-;; org-level-6
-;; org-level-7
-;; org-level-8
-;; org-link
-;; org-meta-line
-;; org-mode-line-clock
-;; org-mode-line-clock-overrun
-;; org-property-value
-;; org-quote
-;; org-scheduled
-;; org-scheduled-previously
-;; org-scheduled-today
-;; org-sexp-date
-;; org-special-keyword
-;; org-table
-;; org-tag
-;; org-target
-;; org-time-grid
-;; org-todo
-;; org-upcoming-deadline
-;; org-verbatim
-;; org-verse
+     ;; org-agenda-column-dateline
+     ;; org-agenda-date
+     ;; org-agenda-date-today
+     ;; org-agenda-date-weekend
+     ;; org-agenda-diary
+     ;; org-agenda-dimmed-todo-face
+     ;; org-agenda-done
+     ;; org-agenda-restriction-lock
+     ;; org-agenda-structure
+     ;; org-archived
+     ;; org-block
+     ;; org-checkbox
+     ;; org-checkbox-statistics-done
+     ;; org-checkbox-statistics-todo
+     ;; org-clock-overlay
+     ;; org-code
+     ;; org-column
+     ;; org-column-title
+     ;; org-date
+     ;; org-document-info
+     ;; org-document-info-keyword
+     ;; org-document-title
+     ;; org-done
+     ;; org-drawer
+     ;; org-ellipsis
+     ;; org-footnote
+     ;; org-formula
+     ;; org-headline-done
+     ;; org-hide
+     ;; org-latex-and-export-specials
+     ;; org-level-1
+     ;; org-level-2
+     ;; org-level-3
+     ;; org-level-4
+     ;; org-level-5
+     ;; org-level-6
+     ;; org-level-7
+     ;; org-level-8
+     ;; org-link
+     ;; org-meta-line
+     ;; org-mode-line-clock
+     ;; org-mode-line-clock-overrun
+     ;; org-property-value
+     ;; org-quote
+     ;; org-scheduled
+     ;; org-scheduled-previously
+     ;; org-scheduled-today
+     ;; org-sexp-date
+     ;; org-special-keyword
+     ;; org-table
+     ;; org-tag
+     ;; org-target
+     ;; org-time-grid
+     ;; org-todo
+     ;; org-upcoming-deadline
+     ;; org-verbatim
+     ;; org-verse
      ;; (org-warning ((t (:foreground ,(colorbrewer-color 'set1 'red))))
      
      )))
